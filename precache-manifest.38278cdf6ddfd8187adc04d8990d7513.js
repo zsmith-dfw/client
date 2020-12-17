0ce4dc35af87810c5cfa66c03a8e78fd430736c4
Zach Smith <65558154+zsmith-dfw@users.noreply.github.com>
2020-12-17 12:31:05 -0600

    Updates

---
+++ precache-manifest.38278cdf6ddfd8187adc04d8990d7513.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efcdfdd14e59f208671f9a88c8a4e5f4",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "33f39499d23c787b53b7",
+    "url": "/client/static/css/main.6a5344e9.chunk.css"
+  },
+  {
+    "revision": "c1e051e806d36358b3d5",
+    "url": "/client/static/js/2.ce550f2d.chunk.js"
+  },
+  {
+    "revision": "de81bfc3d566733bec20fcc6cadc32a6",
+    "url": "/client/static/js/2.ce550f2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33f39499d23c787b53b7",
+    "url": "/client/static/js/main.e732929e.chunk.js"
+  },
+  {
+    "revision": "d64b09180f5230249a46",
+    "url": "/client/static/js/runtime-main.1d4c076d.js"
+  }
+]);
